16afee1211b5a932cba55799ae7200d725b53a66
Deco Oliveira <deco.oliveira@googlemail.com>
2021-04-24 18:22:38 -0300

    episodes page ready

--- src/pages/episodes/[id].tsx
+++ src/pages/episodes/[id].tsx
@@ -1,10 +1,11 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { format, parseISO} from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
-import { useRouter } from 'next/router';
+import Image  from 'next/image';
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
-
+import  styles  from './episode.module.scss';
+import Link from 'next/link';
 type Episode = {
 
   id: string;
@@ -24,7 +25,30 @@ type EpisodeProps = {
 
 export default function Episode({episode}:EpisodeProps){
     return(
-        <h1>  {episode.title}</h1>
+        <div className={ styles.episode}>
+            <div className={styles.thumbnailContainer}>
+                <Link href="/">
+                    <button type="button">
+                        <img src="/arrow-left.svg" alt="voltar"/>
+                    </button>
+                </Link>
+                <Image width={700} height={160} src={episode.thumbnail} objectFit="cover"/>
+                <button type="button">
+                    <img src="/play.svg" alt="Tocar episódio" />
+                </button>
+            </div>
+            <header>
+                <h1>{episode.title}</h1>
+                <span>{episode.members}</span>
+                <span>{episode.publishedAt}</span>
+                <span>{episode.durationAsString}</span>
+            </header>
+
+            <div className={styles.description} 
+                dangerouslySetInnerHTML= {{__html:episode.description}}/>
+                
+            
+        </div>
     )
 }
 
